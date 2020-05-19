84ff740f1cf8f12f6bf398915cc23d318934c607
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-05-18 19:36:01 -0500

    enc

--- src/main/java/sw2/lab5/Entity/Role.java
+++ src/main/java/sw2/lab5/Entity/Role.java
@@ -12,5 +12,19 @@ public class Role {
     private String role_name;
 
 
+    public int getId_rol() {
+        return id_rol;
+    }
 
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
 }
