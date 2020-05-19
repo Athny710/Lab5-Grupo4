8bc19f8b19093738e32e5e2d15b48eccf786073d
Julio Carrion <a20161448@pucp.edu.pe>
2020-05-18 20:12:37 -0500

    juliooooooo5

--- src/main/java/sw2/lab5/Controllers/UsuarioController.java
+++ src/main/java/sw2/lab5/Controllers/UsuarioController.java
@@ -1,7 +1,16 @@
 package sw2.lab5.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/user")
 public class UsuarioController {
+
+    @GetMapping("")
+    public String  info( ){
+        return "User/informacion";
+    }
 }
